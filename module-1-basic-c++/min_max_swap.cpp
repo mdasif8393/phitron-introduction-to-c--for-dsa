28d6e90240cb4598e3f7fa6675c1c626578a1b82
mdasif8393 <md.asif.8393@gmail.com>
2025-11-16 15:02:21 +0600

    swap max min

---
+++ module-1-basic-c++/min_max_swap.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+
+    int a, b;
+    cin >> a>> b;
+
+    cout << min({3,6, 1, 2, 3, 4}) << endl;
+
+    cout << max({5, 8, 4, 7, 8, 2, 9}) << endl;
+
+    swap(a, b);
+    cout << a << " " << b;
+
+    return 0;
+}
