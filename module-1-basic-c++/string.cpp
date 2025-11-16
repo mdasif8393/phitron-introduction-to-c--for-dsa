a2c00fcd7fe0c2aee7b781a7d080f79aa2c83f14
mdasif8393 <md.asif.8393@gmail.com>
2025-11-16 15:26:11 +0600

    string

---
+++ module-1-basic-c++/string.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int x;
+    cin >> x;
+
+    cin.ignore();
+    char s[100];
+    cin.getline(s, 100);
+    cout << x << endl << s;
+
+    string s;
+    
+
+    return 0;
+}
