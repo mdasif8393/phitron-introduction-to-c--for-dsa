41c1d50dde01c85a8d64d913422578a984d3a3be
mdasif8393 <md.asif.8393@gmail.com>
2025-11-17 15:24:19 +0600

    dynamic variable

---
+++ module-2-dynamic-memory-allocation/dynamic-variable.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int* p;
+
+void fun()
+{
+    int *x = new int;
+    *x = 10;
+    p = x;
+    cout << "Fun ->" << *p << endl;
+
+    return;
+}
+
+int main()
+{
+    fun();
+    cout << "Fun ->" << *p << endl;
+
+    return 0;
+}
